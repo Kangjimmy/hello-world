3cc13b6ee748d1349256c836185ba268f93593cf
kisjimmy <skawk3235@gmail.com>
2020-12-18 22:56:34 +0900

    print dan using if,for

--- src/hello/Gugudan2to3.java
+++ src/hello/Gugudan2to3.java
@@ -1,41 +1,31 @@
 package hello;
 
+import java.util.Scanner;
+
 public class Gugudan2to3 {
 
 	public static void main(String[] args) {
-//		System.out.println(2*1);
-//		System.out.println(2*2);
-//		System.out.println(2*3);
-//		System.out.println(2*4);
-//		System.out.println(2*5);
-//		System.out.println(2*6);
-//		System.out.println(2*7);
-//		System.out.println(2*8);
-//		System.out.println(2*9);
+//		사용자가 입력한 숫자 값에 해당하는 단을 출력한다.
+//		예를 들어 사용자가 8이라는 숫자를 입력하면 8단 전체를 계산해 출력한다.
+//		사용자가 2 이상, 9 이하가 아닌 값을 입력하는 경우 "2이상, 9이하의 값만 입력할 수 있습니다."라는 메시지를 출력한다.
 		
-//		int result = 4 * 1;
-//		System.out.println(result);
-//		result = 4 * 2;
-//		System.out.println(result);
-//		result = 4 * 3;
-//		System.out.println(result);
-//		result = 4 * 4;
-//		System.out.println(result);
-//		result = 4 * 5;
-//		System.out.println(result);
-//		result = 4 * 6;
-//		System.out.println(result);
-//		result = 4 * 7;
-//		System.out.println(result);
-//		result = 4 * 8;
-//		System.out.println(result);
-//		result = 4 * 9;
-//		System.out.println(result);
+		Scanner s = new Scanner(System.in);
 		
-		for (int i = 1; i < 10; i++)
+		int dan = s.nextInt();
+		
+		if (dan < 2 || dan > 9)
 		{
-			System.out.println("7 * " + i + " = " + (7 * i));
+			System.out.println("2이상, 9 이하의 값만 입력할 수 있습니다.");
 		}
+		else
+		{
+			for (int i = 1; i < 10; i++)
+			{
+				System.out.println(dan + " * " + i + " = " + (dan*i));
+			}
+		}
+		
+		s.close();
 	}
 
 }
