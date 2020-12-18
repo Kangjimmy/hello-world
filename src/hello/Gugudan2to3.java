5754d4b9d772c7698e895b9949a98285b46e5d88
kisjimmy <skawk3235@gmail.com>
2020-12-18 22:51:02 +0900

    print 7dan

--- src/hello/Gugudan2to3.java
+++ src/hello/Gugudan2to3.java
@@ -13,24 +13,29 @@ public class Gugudan2to3 {
 //		System.out.println(2*8);
 //		System.out.println(2*9);
 		
-		int result = 4 * 1;
-		System.out.println(result);
-		result = 4 * 2;
-		System.out.println(result);
-		result = 4 * 3;
-		System.out.println(result);
-		result = 4 * 4;
-		System.out.println(result);
-		result = 4 * 5;
-		System.out.println(result);
-		result = 4 * 6;
-		System.out.println(result);
-		result = 4 * 7;
-		System.out.println(result);
-		result = 4 * 8;
-		System.out.println(result);
-		result = 4 * 9;
-		System.out.println(result);
+//		int result = 4 * 1;
+//		System.out.println(result);
+//		result = 4 * 2;
+//		System.out.println(result);
+//		result = 4 * 3;
+//		System.out.println(result);
+//		result = 4 * 4;
+//		System.out.println(result);
+//		result = 4 * 5;
+//		System.out.println(result);
+//		result = 4 * 6;
+//		System.out.println(result);
+//		result = 4 * 7;
+//		System.out.println(result);
+//		result = 4 * 8;
+//		System.out.println(result);
+//		result = 4 * 9;
+//		System.out.println(result);
+		
+		for (int i = 1; i < 10; i++)
+		{
+			System.out.println("7 * " + i + " = " + (7 * i));
+		}
 	}
 
 }
