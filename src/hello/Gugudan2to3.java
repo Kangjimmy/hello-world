155c024bc0aa05adae5f165ea2b4509dfa927444
kisjimmy <skawk3235@gmail.com>
2020-12-18 22:46:52 +0900

    printGugudan2

---
+++ src/hello/Gugudan2to3.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class Gugudan2to3 {
+
+	public static void main(String[] args) {
+		System.out.println(2*1);
+		System.out.println(2*2);
+		System.out.println(2*3);
+		System.out.println(2*4);
+		System.out.println(2*5);
+		System.out.println(2*6);
+		System.out.println(2*7);
+		System.out.println(2*8);
+		System.out.println(2*9);
+	}
+
+}
