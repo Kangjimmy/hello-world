a95623c62326757200a09a64b247cc9f7a99e2b4
Kangjimmy <50311129+Kangjimmy@users.noreply.github.com>
2020-12-19 15:11:25 +0900

    update daysOfMonth

--- src/hello/printMonthDay.java
+++ src/hello/printMonthDay.java
@@ -3,24 +3,37 @@ package hello;
 import java.util.Scanner;
 
 public class printMonthDay {
-
+	private final static int[] MAX_DAY_OF_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public static int daysOfMonth(int month) {
+		return MAX_DAY_OF_MONTH[month-1];
+	}
 	public static void main(String[] args) {
-		int[] MonthDay = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("마지막 날을 알고 싶은 월을 입력: ");
-		int month = scanner.nextInt();
-		
-		if (month < 1 || month > 12)
+		while(true)
 		{
-			System.out.println("잘못된 월입니다.");
+			System.out.println("월을 입력하세요");
+			
+			int month = scanner.nextInt();
+			
+			if (month == -1) 
+			{
+				System.out.println("종료합니다.");
+				break;
+			}
+			
+			if (month < 1 || month > 12)
+			{
+				System.out.println("잘못된 입력입니다.");
+				continue;
+			}
+			
+			System.out.println(month+"월은 " + daysOfMonth(month)+"일까지 있습니다.");
 		}
-		else
-		{
-			System.out.println(month + "월은 " + MonthDay[month-1] + "일까지 있습니다.");
-		}
-		
+
 		scanner.close();
 	}
 
