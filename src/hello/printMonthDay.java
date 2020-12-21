3c4cc4a518bf536be8077171afc7250a922ea852
Kangjimmy <50311129+Kangjimmy@users.noreply.github.com>
2020-12-21 20:27:35 +0900

    Implement realCalendar

--- src/hello/printMonthDay.java
+++ src/hello/printMonthDay.java
@@ -1,10 +1,11 @@
 package hello;
 
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class printMonthDay {
 	private final static int[] MAX_DAY_OF_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+	private final static String[] DAY_OF_WEEK = {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
 	public static int daysOfMonth(int month) {
 		return MAX_DAY_OF_MONTH[month-1];
 	}
@@ -21,19 +22,58 @@ public class printMonthDay {
 		
 		return false;
 	}
+	
+	public static void printCalendar(int year, int month) {
+		
+		final int days;
+		if (month == 2 && isYoonYear(year)) {
+			days = 29;
+		}
+		else {
+			days = daysOfMonth(month);
+		}
+		Calendar c = Calendar.getInstance();
+		c.clear();
+		c.set(year, month - 1, 1);
+		int weekDay = c.get(Calendar.DAY_OF_WEEK) - 1;
+
+		System.out.println(month+"월은 " + days+"일까지 있습니다.");
+		System.out.printf("    <%4d년%3d월>%n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
+
+		for (int i = 0; i < weekDay; i++) {
+			System.out.printf("%3s","");
+		}
+		for (int i = 1 ; i <= days; i++) {
+
+			System.out.printf("%3d", i);
+			
+			if ((i+weekDay) % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
-		
-		
+			
 		Scanner scanner = new Scanner(System.in);
 		
 		while(true)
 		{
 			System.out.println("년을 입력하세요");
-			
+			System.out.print("YEAR> ");
 			int year = scanner.nextInt();
+
+			if (year == -1) 
+			{
+				System.out.println("종료합니다.");
+				break;
+			}
 			
 			System.out.println("월을 입력하세요");
-			
+			System.out.print("MONTH> ");
 			int month = scanner.nextInt();
 			
 			if (month == -1) 
@@ -41,38 +81,16 @@ public class printMonthDay {
 				System.out.println("종료합니다.");
 				break;
 			}
-			
+
 			if (month < 1 || month > 12)
 			{
 				System.out.println("잘못된 입력입니다.");
 				continue;
 			}
 			
-			final int days;
-			if (month == 2 && isYoonYear(year)) {
-				days = 29;
-			}
-			else {
-				days = daysOfMonth(month);
-			}
-
-			System.out.println(month+"월은 " + days+"일까지 있습니다.");
-			printCalendar(days);
+			printCalendar(year ,month);
 		}
 
 		scanner.close();
 	}
-	public static void printCalendar(int days) {
-		System.out.println(" SU MO TU WE TH FR SA");
-		System.out.print("---------------------");
-
-		for (int i = 0 ; i < days; i++) {
-			if (i % 7 == 0) {
-				System.out.println();
-			}
-			System.out.printf("%3d", i+1);
-		}
-		System.out.println();
-	}
-
 }
